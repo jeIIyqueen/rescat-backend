2de76529e4da9afc6df40dce0467ce157de048a0
kwonhyeona <gusdk7656@naver.com>
2018-12-23 13:45:21 +0900

    create project

---
+++ src/main/java/org/sopt/appjam/rescat/RescatApplication.java
@@ -0,0 +1,14 @@
+package org.sopt.appjam.rescat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RescatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RescatApplication.class, args);
+	}
+
+}
+
