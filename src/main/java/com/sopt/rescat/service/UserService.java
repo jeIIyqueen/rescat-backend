6ebbc91e2a871fb2cfd55a3bb1493626e91fa9bb
bye0520 <bye0520@naver.com>
2019-01-01 22:52:01 +0900

    merging

--- src/main/java/com/sopt/rescat/service/UserService.java
+++ src/main/java/com/sopt/rescat/service/UserService.java
@@ -1,12 +1,11 @@
 package com.sopt.rescat.service;
 
-import com.sopt.rescat.domain.CareTakerRequest;
-import com.sopt.rescat.domain.Region;
-import com.sopt.rescat.domain.User;
+import com.sopt.rescat.domain.*;
 import com.sopt.rescat.domain.enums.Role;
 import com.sopt.rescat.dto.*;
 import com.sopt.rescat.exception.*;
 import com.sopt.rescat.repository.CareTakerRequestRepository;
+import com.sopt.rescat.repository.ProjectFundingLogRepository;
 import com.sopt.rescat.repository.RegionRepository;
 import com.sopt.rescat.repository.UserRepository;
 import com.sopt.rescat.utils.gabia.com.gabia.api.ApiClass;
@@ -35,6 +34,7 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    private final ProjectFundingLogRepository projectFundingLogRepository;
     private final CareTakerRequestRepository careTakerRequestRepository;
     private final S3FileService s3FileService;
     private final RegionRepository regionRepository;
@@ -49,13 +49,15 @@ public class UserService {
 
     public UserService(final UserRepository userRepository, final PasswordEncoder passwordEncoder,
                        final CareTakerRequestRepository careTakerRequestRepository, S3FileService s3FileService,
-                       final RegionRepository regionRepository, final MapService mapService) {
+                       final RegionRepository regionRepository, final MapService mapService,
+                       final ProjectFundingLogRepository projectFundingLogRepository) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.careTakerRequestRepository = careTakerRequestRepository;
         this.s3FileService = s3FileService;
         this.regionRepository = regionRepository;
         this.mapService = mapService;
+        this.projectFundingLogRepository = projectFundingLogRepository;
     }
 
     public Boolean isExistingId(String id) {
@@ -155,5 +157,19 @@ public class UserService {
             throw new AlreadyExistsException("newPassword", "현재 사용중인 PASSWORD입니다.");
         if(userPasswordDto.checkValidPassword())
             user.updatePassword(passwordEncoder.encode(userPasswordDto.getNewPassword()));
+    }
+
+    public List<Funding> getSupportingFundings(User user) {
+
+        List<ProjectFundingLog> projectFundingLogs = projectFundingLogRepository.findBySponsorOrderByCreatedAtDesc(user);
+
+        return getFundingsByLogs(projectFundingLogs);
+    }
+
+    private List<Funding> getFundingsByLogs(List<ProjectFundingLog> projectFundingLogs){
+        return projectFundingLogs.stream()
+                .map(ProjectFundingLog::getFunding)
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
