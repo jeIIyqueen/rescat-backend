ddfe26a649174905fd547ae193d997854844a8d5
kwonhyeona <gusdk7656@naver.com>
2019-01-10 00:50:39 +0900

    [CREATE] String to LocalDate converter, apply session at admin api

--- src/main/java/com/sopt/rescat/config/WebMvcConfig.java
+++ src/main/java/com/sopt/rescat/config/WebMvcConfig.java
@@ -1,10 +1,13 @@
 package com.sopt.rescat.config;
 
 import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
+import com.sopt.rescat.converter.LocalDateConverter;
+import com.sopt.rescat.converter.LocalDateTimeConverter;
 import com.sopt.rescat.filter.RequestBodyXSSFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
@@ -12,6 +15,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new LocalDateConverter("yyyy-MM-dd"));
+        registry.addConverter(new LocalDateTimeConverter("yyyy-MM-dd HH:mm:ss"));
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -19,8 +29,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Bean
     public MethodValidationPostProcessor methodValidationPostProcessor() {
-        MethodValidationPostProcessor processor = new MethodValidationPostProcessor();
-        return processor;
+        return new MethodValidationPostProcessor();
     }
 
     @Bean
@@ -39,5 +48,4 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registrationBean.addUrlPatterns("/api/admin");
         return registrationBean;
     }
-
 }
